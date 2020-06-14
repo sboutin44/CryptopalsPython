a6568756b49c3cb1918a425f82e9b3d1e7c7dc77
Sebastien Boutin <sboutin44@me.com>
2020-06-14 13:32:26 -0700

    Added virtual environment.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # CryptopalsPython
+
+## Required Packges
+
+`$ pip3 install pycrypto`
+
+
+## Run the challenges
+
+To run a challenge, eg. the 16-th:
+
+1. Go to the project root directory:
+
+     `$ cd Cryptopals`
+
+1. Activate the Python virtual environment: 
+
+    `$ source venv/bin/activate`
+
+1. Launch the desired challenge:
+
+    `$ python src/16.py`
+
+1. When finished, deactivate the virtual environment:
+
+    `$ deactivate`
+    
--- src/16.py
+++ src/16.py
@@ -1,7 +1,31 @@
+# // Copyright © 2020 Sebastien BOUTIN
+# //
+# // Permission is hereby granted, free of charge, to any person obtaining a copy
+# // of this software and associated documentation files (the "Software"), to
+# // deal in the Software without restriction, including without limitation the
+# // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# // sell copies of the Software, and to permit persons to whom the Software is
+# // furnished to do so, subject to the following conditions:
+# //
+# // The above copyright notice and this permission notice shall be included in
+# // all copies or substantial portions of the Software.
+# //
+# // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# // IN THE SOFTWARE.
+# //
+# // Except as contained in this notice, the name(s) of the above copyright
+# // holders shall not be used in advertising or otherwise to promote the sale,
+# // use or other dealings in this Software without prior written authorization.
+
 from Crypto.Random import get_random_bytes
 from copy import copy
 
-# My functions
+# My Cryptopals functions
 from block_ciphers import PKCS7_doPadding
 from block_ciphers import AES_CBC_encrypt
 from block_ciphers import AES_CBC_decrypt
@@ -55,7 +79,4 @@ if __name__ == "__main__":
     print(ciphertext)
     deciphered  = f2(ciphertext, key, iv)
     print(deciphered)
-
-
-    # printHEX(PKCS7_doPadding(b'coucou'))
 
